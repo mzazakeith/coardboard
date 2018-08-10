4b54f0802c14b92b3dad77599b832bb56403402c
mzazakeith <mzazakeith@gmail.com>
2018-08-10 10:17:56 +0300

    upload care implemented

--- authentication/models.py
+++ authentication/models.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+from pyuploadcare.dj.models import ImageField
+
 Gender_Choices = (
     ('F', 'female'),
     ('M', 'male'),
@@ -21,7 +24,7 @@ class Messages(models.Model):
 
 
 class TeacherProfile(models.Model):
-    # profile_pic = ImageField()
+    profile_pic = ImageField()
     bio = models.TextField()
     gender = models.CharField(max_length=30, choices=Gender_Choices, default='None', blank=True)
     phone_number = models.PositiveIntegerField()
@@ -30,7 +33,7 @@ class TeacherProfile(models.Model):
 
 
 class StudentProfile(models.Model):
-    # profile_pic = ImageField()
+    profile_pic = ImageField()
     gender = models.CharField(max_length=30, choices=Gender_Choices, default='None', blank=True)
     phone_number = models.PositiveIntegerField()
     user = models.OneToOneField(User)
--- codeboard/settings.py
+++ codeboard/settings.py
@@ -84,6 +84,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -121,4 +128,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "authentication.User"
+
+UPLOADCARE = {
+    'pub_key': '45b9d1d695652a0bb169',
+    'secret': 'e2d8fb3eb2efde281794',
+}
+
 
